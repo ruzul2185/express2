0cf0d854d852ba321827feb4d9cbb10bb8c7450c
ruzul <ruzulchaure2185@gmail.com>
2025-10-30 18:16:35 +0530

    folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
+import { config } from "dotenv";
+config();
+
 import express from "express";
 
-const app = express();
+import type { Request, Response } from "express";
 
-app.listen(5000, () => {
-  console.log("Server is running of port 5000!");
+const app = express();
+app.use(express.json());
+
+app.use("/:something", (req: Request, res: Response) => {
+  const { something } = req.params;
+  return res.status(200).json({ message: "Successful Response!" + something });
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log("Server is running of port " + PORT);
 });
